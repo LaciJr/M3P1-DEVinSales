539d97bfaae0b56a3dc0e365785ed3e31bbbc4dc
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-06-07 22:43:30 -0300

    Fix: Altera seeds para novos profiles

--- DevInSales/Seeds/ProfileSeed.cs
+++ DevInSales/Seeds/ProfileSeed.cs
@@ -1,9 +1,10 @@
 ﻿using DevInSales.Models;
+using DevInSales.Enums;
 
 namespace DevInSales.Seeds
 {
     public class ProfileSeed
     {
-        public static List<Profile> Seed { get; set; } = new List<Profile>() { new Profile(1, "Cliente") };
+        public static List<Profile> Seed { get; set; } = new List<Profile>() { new Profile(1, "Usuário",PermissoesEnum.Usuario), new Profile(2, "Gerente",PermissoesEnum.Gerente), new Profile(3, "Administrador", PermissoesEnum.Administrador) };
     }
 }
