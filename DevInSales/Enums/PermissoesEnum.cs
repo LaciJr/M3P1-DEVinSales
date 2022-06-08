35cd9f9073bc637eabac8f9de2fad898f46e6d57
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-06-07 22:06:26 -0300

    Feat: Adiciona Roles de permissionamento

---
+++ DevInSales/Enums/PermissoesEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevInSales.Enums
+{
+    public enum PermissoesEnum
+    {
+        [Display(Name ="Usuário")]
+        Usuario = 1,
+        [Display(Name = "Gerente")]
+        Gerente,
+        [Display(Name = "Administrador")]
+        Administrador
+    }
+}
