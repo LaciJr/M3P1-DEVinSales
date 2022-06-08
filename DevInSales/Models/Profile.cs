234d1361e75f3b694f5d06a0c603b1c274411839
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-06-07 22:43:03 -0300

    Fix: Altera entidade Profile para adicionar o permissionamento de usuários

--- DevInSales/Models/Profile.cs
+++ DevInSales/Models/Profile.cs
@@ -1,3 +1,5 @@
+using DevInSales.Enums;
+using Microsoft.OpenApi.Extensions;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DevInSales.Models;
@@ -6,16 +8,19 @@ public class Profile
 {
     [Column("id")]
     public int Id { get; set; }
-    [Column("name")]
-    public string Name{ get; set; }
+    [Column("Role")]
+    public string Role { get; set; }
+    [Column("Permissão")]
+    public PermissoesEnum Permissao { get; set; }
 
     public Profile()
     {
     }
 
-    public Profile(int id, string name)
+    public Profile(int id, string role,PermissoesEnum permissao)
     {
         Id = id;
-        Name = name;
+        Permissao = permissao;
+        Role = role;
     }
 }
